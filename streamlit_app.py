89341180eafc0362364a1f8eaf7289a8e0ef88c3
Meryam <116731707+MeryamBahoussi@users.noreply.github.com>
2023-02-14 15:16:19 +0100

    Update streamlit_app.py
    added capital lettrer to mom
    changed header
    added emojis and a text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title("My mom's New Healthy Diner")
+streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
